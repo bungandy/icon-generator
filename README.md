7b74864096cba4ede455fe13e453009824e2363d
Andy <bungandy@gmail.com>
2022-10-06 08:17:40 +0700

    bismillah

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # icon-generator
 Icon generator for favicon
+
+Run: `npm run build
+
+Output:
+```
+icon-16x16.png ✅
+icon-32x32.png ✅
+icon-57x57.png ✅
+icon-60x60.png ✅
+icon-72x72.png ✅
+icon-76x76.png ✅
+icon-96x96.png ✅
+icon-114x114.png ✅
+icon-120x120.png ✅
+icon-144x144.png ✅
+icon-152x152.png ✅
+icon-180x180.png ✅
+icon-192x192.png ✅
+icon-194x194.png ✅
+icon.png ✅
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const resizeImg = require('resize-img');
+ 
+(async () => {
+  const file = 'src/icon.png'
+  const dest = 'icon'
+  const sizes = [16,32,57,60,72,76,96,114,120,144,152,180,192,194]
+
+  if (!fs.existsSync('icon')) {
+    fs.mkdirSync('icon')
+  }
+
+  for (i of sizes){
+    console.log(`icon-${i}x${i}.png ✅`)
+
+    const image = await resizeImg(fs.readFileSync(file), {
+      width: i,
+      height: i
+    })
+
+    fs.writeFileSync(`${dest}/icon-${i}.png`, image)
+  }
+
+  fs.copyFile(file, `${dest}/icon.png`, (err) => {
+    if (err) {
+      console.log("Oops! An Error Occured:", err);
+    }
+    else {
+      console.log('icon.png ✅');
+    }
+  });
+  
+})();
